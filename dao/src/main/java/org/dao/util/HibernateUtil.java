8853be1d3a767dd8870cd9315838601e5bed8e79
Константин <k336699k@mail.ru>
2015-09-07 11:04:14 +0300

    hib 4

--- dao/src/main/java/org/dao/util/HibernateUtil.java
+++ dao/src/main/java/org/dao/util/HibernateUtil.java
@@ -6,28 +6,17 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
+	  private static final SessionFactory sessionFactory;
+	    static {
+	      try {
+	        sessionFactory = new Configuration().configure().buildSessionFactory();
+	      } catch (Throwable ex) {
+	        System.err.println("Initial SessionFactory creation failed." + ex);
+	        throw new ExceptionInInitializerError(ex);
+	      }
+	    }
 
-    private static final SessionFactory sessionFactory = buildSessionFactory();
-
-    private static SessionFactory buildSessionFactory() {
-        try {
-            // Create the SessionFactory from hibernate.cfg.xml
-        	Configuration configuration = new Configuration().configure();
-        	StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().
-        	applySettings(configuration.getProperties());
-        	SessionFactory factory = configuration.buildSessionFactory(builder.build());
-        	
-        	return factory;
-        }
-        catch (Throwable ex) {
-            // Make sure you log the exception, as it might be swallowed
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
-
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
-
-}
+	    public static SessionFactory getSessionFactory() {
+	      return sessionFactory;
+	    }
+	}
